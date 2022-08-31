7e37978da2c7eef001bb25c6ca0388b8245feb0d
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2022-08-31 13:13:48 +0300

    animation had been added

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -8,6 +8,14 @@ import Achievements from './Components/Achievements';
 import './styles.css';
 
 const App = () => {
+  document.addEventListener('mousemove', e => {
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+    const x = Math.round(e.pageX / w * 100);
+    const y = Math.round(e.pageY / h * 100);
+    document.body.style.background = `radial-gradient(at ${x}% ${y}%, rgba(10, 73, 128, 1), rgba(0, 0, 0, 1))`;
+  });
+
   return (
     <div className='App'>
       <div className='NavBar'>
